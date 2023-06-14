ee60b30112199e24c0694b3c71c26d69f11bd130
evrard29 <evrard715@gmail.com>
2023-06-14 12:12:13 +0100

    ajout des lignes6 et 22 dans providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvi
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
